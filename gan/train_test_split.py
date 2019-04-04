8867481c57d244373526adcde771e5968d510264
Matt <zhaozihaotongji@gmail.com>
2019-04-04 17:07:17 +0800

    gan

---
+++ gan/train_test_split.py
@@ -0,0 +1,17 @@
+import numpy as np
+from torch.utils.data.sampler import SubsetRandomSampler
+
+def train_test_split(dataset, validation_split=0.2,shuffle_dataset = True,random_seed= 42):
+    # Creating data indices for training and validation splits:
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(validation_split * dataset_size))
+    if shuffle_dataset :
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+    return train_sampler, valid_sampler
